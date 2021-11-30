caa087cb3ff1a39adab23c4bc47a6ce09c66fa07
scott-hamp <scotthamp.toronto@gmail.com>
2021-11-30 17:23:12 +0800

    Updated.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -64,8 +64,8 @@ var _boardThemes =
     },
     {
         name: "Stone",
-        light: "rgba(188, 179, 165, 0.4)", 
-        dark: "rgba(38, 36, 39, 0.5)",
+        light: "rgba(188, 179, 165, 0.55)", 
+        dark: "rgba(38, 36, 39, 0.55)",
         backgroundImage: "assets/images/background-stone-0.png",
         labelLight: "rgb(208, 199, 185)",
         labelDark: "rgb(98, 96, 99)"
