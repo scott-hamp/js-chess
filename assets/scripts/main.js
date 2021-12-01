7780d4dbc563931f6e9bcff6daeb903884cc45c6
scott-hamp <scotthamp.toronto@gmail.com>
2021-12-01 12:45:43 +0800

    Added saving the board to an image.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1305,6 +1305,42 @@ function resetMoveClocks()
         timeSpans[i].textContent = `${time}:00`;
 }
 
+function saveBoardAsImage()
+{
+    function getScreenshotOfElement(element, posX, posY, width, height, callback) 
+    {
+        html2canvas(element, {
+            onrendered: function (canvas) 
+            {
+                var context = canvas.getContext('2d');
+                var imageData = context.getImageData(posX, posY, width, height).data;
+                var outputCanvas = document.createElement('canvas');
+                var outputContext = outputCanvas.getContext('2d');
+                outputCanvas.width = width;
+                outputCanvas.height = height;
+    
+                var idata = outputContext.createImageData(width, height);
+                idata.data.set(imageData);
+                outputContext.putImageData(idata, 0, 0);
+                callback(outputCanvas.toDataURL().replace("data:image/png;base64,", ""));
+            },
+            width: width,
+            height: height,
+            useCORS: true,
+            taintTest: false,
+            allowTaint: false
+        });
+    }
+
+    var boardDiv = document.getElementById("board-div");
+    var boardDivRect = boardDiv.getBoundingClientRect();
+    var boardDivSize = (boardDivRect.right - boardDivRect.left);
+
+    getScreenshotOfElement(boardDiv, 0, 0, boardDivSize, boardDivSize, (data) => {
+        alert(data);
+    });
+}
+
 function saveGame() 
 {
     var content = "";
@@ -2091,7 +2127,12 @@ function controlsGameButton_onClick(descriptor)
     if(descriptor == "reset")
     {
         resetLoadedGame();
+        return;
+    }
 
+    if(descriptor == "save-image")
+    {
+        saveBoardAsImage();
         return;
     }
 }
